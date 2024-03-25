c2b5ce832f1610821180524dea0b333faaa54738
Alex Faustino <faustino.alex@outlook.com>
2024-03-25 20:12:51 -0300

    feat: "add function file-metadata"

--- fcc-project-filemetadata/index.js
+++ fcc-project-filemetadata/index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs')
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,49 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+setupMulter();
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  const filePath = req.file.path;
+  const fileContent = fs.readFileSync(filePath, { encoding: 'base64' });
+
+  fs.unlinkSync(filePath);
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ name: originalname, type: mimetype, size: size, content: fileContent });
+});
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+function setupMulter() {
+  const uploadDir = path.join(__dirname, 'uploads');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  
+  app.locals.upload = multer({ storage: storage });
+}
